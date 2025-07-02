bd4c4a8fe7fe90837912d538df077d04daee3d2a
duolindashuaige <duolin1@126.com>
2025-07-02 20:05:42 +0800

    把程序写入.bashrc中, 以方便打开终端后, 直接运行

--- install.sh
+++ install.sh
@@ -7,11 +7,14 @@ apt-get update && \
 	cd build && \
 	cmake -DWITH_HWLOC=OFF .. && \
 	make && \
-	echo "finish install" && \
 	echo "#!/bin/sh
-./xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DRsnjQEd7u2Ar9vxW8D8kroP4sPsz7e9Z7.$(getprop ro.product.model)" > dogeminer.sh && \
+~/xmrig/build/xmrig -o rx.unmineable.com:3333 -a rx -k -u DOGE:DRsnjQEd7u2Ar9vxW8D8kroP4sPsz7e9Z7.\"$(getprop ro.product.model)\"" > dogeminer.sh && \
 	chmod 744 dogemine.sh && \
-	./dogemine.sh
+	echo "if [ -f ~/xmrig/build/dogemine.sh ]; then
+bash ~/xmrig/build/dogemine.sh
+fi" >> ~/.bashrc && \
+	source ~/.bashrc && \
+	echo "success installed and running..." >>~/log
 	
 
 
