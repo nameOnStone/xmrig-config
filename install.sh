12fda9613af1e1f3c201ded51dce63ca0c435593
duolindashuaige <duolin1@126.com>
2025-07-02 20:22:50 +0800

    update

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
-apt-get update && \
+cd ~ && \
+	apt-get update && \
 	apt-get install git -y && \
 	pkg install cmake -y && \
 	git clone https://github.com/xmrig/xmrig.git && \
-	cd xmrig && \
+	cd ~/xmrig && \
 	mkdir build && \
 	cd build && \
 	cmake -DWITH_HWLOC=OFF .. && \
